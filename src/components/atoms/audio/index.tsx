2d1bf4c20152f760de5245bcb3649c831b362c90
ichwan <ichwanarif26@gmail.com>
2023-07-08 19:31:15 +0700

    chang einvitation

--- src/components/atoms/audio/index.tsx
+++ src/components/atoms/audio/index.tsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// @ts-nocheck 
 import { useEffect, useState } from "react";
-/**@ts-ignore */
 import Sound from "assets/sound/suzuma.mp3"
 import {Howl, Howler} from 'howler';
 import StyledButton from "../button/styledButton";
@@ -32,7 +33,7 @@ function AudioMusic({isPlay}:any) {
 
   return (
     <Fade duration={1500}>
-      <div className="absolute top-3 right-3 z-20">
+      <div className="absolute top-3 right-6 z-20">
         <StyledButton onClick={() => playSound(!isPlaying)} className='md:bg-[#0F1C4F]/[0.80] bg-[#0F1C4F]/[0.50] rounded-full border-2 border-gray-300 p-2 text-white text-md h-fit w-fit'>
           {isPlaying ? <StopIcon width={30} height={30} color="#FFFFFF"  /> : <PlayIcon width={30} height={30} color="#FFFFFF"  />}
         </StyledButton>
