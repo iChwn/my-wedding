0005f517dcf1f464831834e93b478f4f8d8f0dc7
ichwan <ichwanarif26@gmail.com>
2023-07-10 09:17:13 +0700

    asdwdwd

--- src/page/home/opening.tsx
+++ src/page/home/opening.tsx
@@ -3,11 +3,18 @@ import { useRive, Layout, Fit, Alignment } from '@rive-app/react-canvas';
 import { useEffect, useState } from "react";
 import { useMediaQuery } from 'utility/helper';
 import "assets/css/custom.scss"
+import { useLocation } from 'react-router';
+import { decryptId } from 'utility/helper/generateInvitation';
+import guest_list from 'constant/guestList';
 // import { useNavigate } from 'react-router';
 
 const Opening = ({callback}:any) => {
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search);
+  const decryptedId = decryptId(queryParams.get('userid'))
+  const isValidGuset = guest_list.find((guest) => guest.id === decryptedId)
+  const [isShow, setIsShow] = useState(true)
   const [isExit, setIsExit] = useState(false)
-  // const navigate = useNavigate()
   const smQuery = useMediaQuery("only screen and (min-width: 700px)");
   // const mdQuery = useMediaQuery("only screen and (min-width: 1025px)");
   const { rive, RiveComponent } = useRive({
@@ -25,11 +32,12 @@ const Opening = ({callback}:any) => {
         setIsExit(true)
         setTimeout(() => {
           callback("mantap")
-        }, 2000);
-      } 
+        }, 1500);
+      } else if(state === "OnReadyToFly") {
+        setIsShow(false)
+      }
     },
   });
-  // const isHoverInput = useStateMachineInput(rive, "State Machine 1", "PointerIn");
 
   useEffect(() => {
     if (rive) {
@@ -46,17 +54,14 @@ const Opening = ({callback}:any) => {
       }
     }
   }, [rive, smQuery]);
-
-  // useEffect(() => {
-  //   if (rive && isHoverInput) {
-  //     isHoverInput.value = true;
-  //   }
-  // }, [isHoverInput])
  
   return (
-    <div className="h-full w-full flex items-center justify-center flex-col">
+    <div className="h-full w-full flex items-center justify-center flex-col relative bg-[#ffebc0]">
       <div id="transition" className={`circle-white circle-white-scale ${isExit && 'scale-circle'}`} style={{top: 30}}></div>
       <RiveComponent />
+      {isShow && (
+        <div className='bg-white font-black-hole text-black px-3 py-1 rounded-md absolute bottom-[10px] uppercase font-bold text-[20px] border-2 border-black'>Untuk {isValidGuset.name}</div>
+      )}
     </div>
   )
 }
